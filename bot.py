85882f1226779c73f1fcadf473cf0a5f034d7787
letomaneteo <95133883+letomaneteo@users.noreply.github.com>
2025-01-19 16:59:35 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,19 +1,19 @@
-import os
 from flask import Flask, request
 from telegram import Bot, Update
-import logging
 
 app = Flask(__name__)
 
-# Telegram token
-TOKEN = '7815366595:AAGA-HPHVPqyTQn579uoeM7yPDRrf-UIdsU'
+# Установите ваш токен для бота
+TOKEN = "YOUR_TELEGRAM_BOT_TOKEN"  # Замените на ваш токен
+WEBHOOK_URL = "https://flask-production-b6fb.up.railway.app/webhook"  # Прямая ссылка
+
+# Инициализация бота
 bot = Bot(token=TOKEN)
 
 # Установка вебхука
-webhook_url = 'https://flask-production-b6fb.up.railway.app/webhook'  # Используйте ваш реальный URL с Railway
-bot.set_webhook(url=webhook_url)
+bot.set_webhook(url=WEBHOOK_URL)
 
-# Вебхук для обработки запросов от Telegram
+# Обработка вебхука
 @app.route('/webhook', methods=['POST'])
 def webhook():
     try:
@@ -25,9 +25,9 @@ def webhook():
 
         return "OK", 200
     except Exception as e:
-        logging.error(f"Ошибка: {e}")
-        return "Error", 500
+        return f"Error: {e}", 500
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
+
 
