7efc73e60990ab29609d7c5cf0284e59dd7a4324
letomaneteo <95133883+letomaneteo@users.noreply.github.com>
2025-01-19 17:49:44 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,38 +1,30 @@
 from flask import Flask, request
-from telegram import Bot, Update
-import asyncio
+from telegram import Bot
 
 app = Flask(__name__)
 
 # Установите ваш токен для бота
 TOKEN = "7815366595:AAGA-HPHVPqyTQn579uoeM7yPDRrf-UIdsU"  # Замените на ваш токен
-WEBHOOK_URL = "https://web-production-aa772.up.railway.app/webhook"  # Прямая ссылка
+WEBHOOK_URL = "https://flask-production-b6fb.up.railway.app/webhook"  # Прямая ссылка
 
 # Инициализация бота
 bot = Bot(token=TOKEN)
 
 # Установка вебхука
-async def set_webhook():
-    await bot.set_webhook(url=WEBHOOK_URL)
+bot.set_webhook(url=WEBHOOK_URL)
 
-@app.before_first_request
-def before_first_request():
-    loop = asyncio.get_event_loop()
-    loop.run_until_complete(set_webhook())
-
-# Обработка вебхука
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    try:
-        # Получаем обновление от Telegram
-        update = Update.de_json(request.get_json(), bot)
+    data = request.get_json()
 
-        # Ответ на сообщение
-        bot.send_message(chat_id=update.message.chat_id, text="Привет, бот работает!")
+    # Проверяем, что в сообщении команда /start
+    if "message" in data and "text" in data["message"]:
+        text = data["message"]["text"]
+        if text == "/start":
+            chat_id = data["message"]["chat"]["id"]
+            bot.send_message(chat_id=chat_id, text="Привет, я бот!")
 
-        return "OK", 200
-    except Exception as e:
-        return f"Error: {e}", 500
+    return "OK", 200
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8080)
