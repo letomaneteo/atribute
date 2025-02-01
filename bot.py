5ed1acd0bf27c4ce09f0b763806153ee08a4a5bc
letomaneteo <95133883+letomaneteo@users.noreply.github.com>
2025-02-01 16:50:37 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -146,4 +146,5 @@ def chat_with_deepseek(user_message):
 
         
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8080)
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv("PORT", 8080)))
+
